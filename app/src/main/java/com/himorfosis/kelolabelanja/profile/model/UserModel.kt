65133fd6183ea5b645d51d8ec3672359a0cedcb5
himawan <haimar404@gmail.com>
2020-05-19 10:49:46 +0700

    user model

--- app/src/main/java/com/himorfosis/kelolabelanja/profile/model/UserModel.kt
+++ app/src/main/java/com/himorfosis/kelolabelanja/profile/model/UserModel.kt
@@ -1,4 +1,16 @@
 package com.himorfosis.kelolabelanja.profile.model
 
-class UserModel {
-}
+class UserModel (
+        var id: String,
+        val name: String,
+        val email: String,
+        val phone_number: String,
+        val image: String,
+        val image_url: String,
+        val gender: String,
+        val born: String,
+        val token: String,
+        val active: Int,
+        val created_at: String,
+        val updated_at: String
+)
--- app/src/main/java/com/himorfosis/kelolabelanja/profile/model/UserUpdateRequest.kt
+++ app/src/main/java/com/himorfosis/kelolabelanja/profile/model/UserUpdateRequest.kt
@@ -1,4 +1,10 @@
 package com.himorfosis.kelolabelanja.profile.model
 
-class UserUpdateRequest {
-}
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.http.PartMap
+
+class UserUpdateRequest (
+        var body: MultipartBody.Part,
+        var map: Map<String, RequestBody>
+)
